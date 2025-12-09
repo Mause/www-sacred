064609ceb5fb86f28f20c92ee55abfbd78326666
Elliana May <me@mause.me>
2025-12-09 16:39:41 +0800

    vite

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [
+    dts({
+      tsconfigPath: resolve(__dirname, 'tsconfig.json'),
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, './components/index.tsx'),
+      name: 'srcl',
+      // the proper extensions will be added
+      fileName: 'scrl',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          'react-dom': 'ReactDOM',
+          react: 'React',
+        },
+      },
+    },
+  },
+});
