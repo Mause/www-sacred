9b5e33f811a49836550ca867aed4b48eddeb8d54
Elliana May <me@mause.me>
2025-12-11 18:11:49 +0800

    ModalContext

--- gen.py
+++ gen.py
@@ -6,6 +6,9 @@ out = ""
 for file in sorted(p.glob("**/*.tsx")):
     if file.stem == "index":
         continue
+    elif file.stem == "ModalContext":
+        out += f"export {{ {file.stem}, useModals }} from './{file.relative_to(p).with_suffix('')}';\n"
+        continue
     out += f"export {{ default as {file.stem} }} from './{file.relative_to(p).with_suffix('')}';\n"
 
 with (p / "index.tsx").open("w") as f:
